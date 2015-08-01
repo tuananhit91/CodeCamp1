4e30426c2fd44c91f12a31947e62a7d17778a3a9
Hoang Tuan Anh <tuananhit91@gmail.com>
2015-08-02 00:34:50 +0700

    Initial Commit

---
+++ CodeCamp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodeCamp1
+//
+//  Created by admin on 8/2/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodeCamp1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodeCamp1
+//
+//  Created by admin on 8/2/15.
+//  Copyright (c) 2015 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
